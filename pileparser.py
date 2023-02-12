354019e8c88dc3f18386f670b628fb428eccd3b5
Frederick De Baene <frederick.debaene@hotmail.com>
2023-02-12 19:05:29 +0100

    Define additional attributes

--- pileparser.py
+++ pileparser.py
@@ -3,6 +3,14 @@ import os
 
 class PileParser:
 
-    def __init__(self, file_path):
+    def __init__(self, file_path, no_workers):
         # Initialize the attributes for the object
         self._file_path = file_path
+
+        try:
+            self._file_size = os.path.getsize(self._file_path)
+        except FileNotFoundError as e:
+            print(e)
+
+        self._no_workers = no_workers
+        self._chunk_bounds = []
